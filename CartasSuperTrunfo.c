600df3dcf82df57127d5b8aa998806abfce99f5f
Junior37373 <juninho19gilberto19@GMAIL.COM>
2026-02-25 12:12:23 +0000

    Completei o nível novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,72 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+char estado[20]; 
+char codigo[20];
+char cidade[30];
+int populacao;
+float area;
+int turistico;
+float pib; 
 
-  // Área para entrada de dados
 
-  // Área para exibição dos dados da cidade
+printf("Carta 1:\nUma letra de 'A' a 'H' (representando um dos oito estados): ");
+scanf("%s", &estado);
+
+printf("A letra do estado seguida de um número de 01 a 04 (ex: A01, B03): ");
+scanf("%s", &codigo);
+
+printf("O nome da cidade: ");
+scanf("%s", &cidade);
+
+printf("Qual é o número de habitantes da cidade: ");
+scanf("%d", &populacao);
+
+printf("A área da cidade em quilômetros quadrados: ");
+scanf("%f", &area);
+
+printf("O Produto Interno Bruto(PIB) da cidade: ");
+scanf("%f", &pib);
+
+printf("Qual a quantidade de pontos turísticos na cidade: ");
+scanf("%d", &turistico);
+
+
+
+char Bahia[20];
+char cod[20];
+char Municipiu[30]; 
+int pessoas;
+float tamanho; 
+float PIB;
+int turismo;
+
+printf("Carta 2:\nUma letra de 'A' a 'H' (representando um dos oito estados): ");
+scanf("%s", &Bahia);
+
+printf("A letra do estado seguida de um número de 01 a 04 (ex: A01, B03): ");
+scanf("%s", &cod);
+
+printf("O nome da cidade: ");
+scanf("%s", &Municipiu);
+
+printf("Qual é o número de habitantes da cidade: ");
+scanf("%d", &pessoas);
+
+printf("A área da cidade em quilômetros quadrados: ");
+scanf("%f", &tamanho);
+
+printf("O Produto Interno Bruto(PIB) da cidade: ");
+scanf("%f", &PIB);
+
+printf("Qual a quantidade de pontos turísticos na cidade: ");
+scanf("%d", &turismo);
+
+printf("Carta 1: \n Estado: %s\n Código: %s\n Nome da Cidade: %s\n População: %d\n Área: %f\n PIB: %f\n Número de Pontos Turísticos: %d\n", estado, codigo, cidade, populacao, area, pib, turistico);
+
+printf("Carta 2: \n Estado: %s\n Código: %s\n Nome da Cidade: %s\n População: %d\n Área: %f\n PIB: %f\n Número de Pontos Turísticos: %d\n", Bahia, cod, Municipiu, pessoas, tamanho, PIB, turismo);
 
 return 0;
-} 
+
+
+}
