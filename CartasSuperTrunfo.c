d24e4d7ae3d9673ead37802fb70148ab271777a2
Junior37373 <juninho19gilberto19@GMAIL.COM>
2026-02-26 17:40:43 +0000

    Completei o Desafio nível mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@ int main() {
 char estado[20]; 
 char codigo[20];
 char cidade[30];
-int populacao;
+unsigned long int populacao;
 float area;
 int turistico;
 float pib; 
@@ -36,7 +36,7 @@ scanf("%d", &turistico);
 char Bahia[20];
 char cod[20];
 char Municipiu[30]; 
-int pessoas;
+unsigned long int pessoas;
 float tamanho; 
 float PIB;
 int turismo;
@@ -73,11 +73,25 @@ densidade = populacao / area;
 Densidade = pessoas / tamanho;
 per = pib / populacao;
 Per = PIB / pessoas ;
+float super;
+float super1;
+float densidadeinversa;
+float densidadeinversa1;
 
 
 printf("Carta 1: \n Estado: %s\n Código: %s\n Nome da Cidade: %s\n População: %d\n Área: %f\n PIB: %f\n Número de Pontos Turísticos: %d\n Densidade Populacional: %.2f\n PIB per Capita: %.2f\n", estado, codigo, cidade, populacao, area, pib, turistico, densidade, per);
 
 printf("Carta 2: \n Estado: %s\n Código: %s\n Nome da Cidade: %s\n População: %d\n Área: %f\n PIB: %f\n Número de Pontos Turísticos: %d\n Densidade Populacional: %.2f\n PIB per Capita: %.2f\n", Bahia, cod, Municipiu, pessoas, tamanho, PIB, turismo, Densidade, Per);
+
+densidadeinversa = 1.0 / densidade; 
+densidadeinversa1 = 1.0 / Densidade;
+
+super = (float) populacao + area + pib + turistico + per + densidadeinversa;
+
+super1 = (float) pessoas + tamanho + PIB + turismo + Per + densidadeinversa1;
+printf("Comparação de Cartas: \n População: %d \n Área: %d \n Pib: %d \n Pontos turisticos: %d \n Densidade Populacional: %d \n PIB per Capita: %d \n Super Poder: %d ", populacao > pessoas, area > tamanho, pib > PIB, turistico > turismo, densidade < Densidade, per > Per, super > super1);
+
+
 
 return 0;
 
